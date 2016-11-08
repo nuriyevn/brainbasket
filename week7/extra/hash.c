bad692e6bc4fc65c3edd776740cd7f4878ee8fbc
nuriyevn <nuriyevn@gmail.com>
2016-11-08 21:26:49 +0200

    Solving collision problem for hash.c using linked list

--- week7/extra/hash.c
+++ week7/extra/hash.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <cs50.h>
+#include "cs50.h"
 #include <limits.h>
 
 typedef struct node_s {
@@ -11,16 +11,16 @@ typedef struct node_s {
 } node ;
 
 typedef struct hashtable_s {
-    int size ; 
+    int size ;
     struct node_s **table ;
 } hashtable ;
 
 hashtable* hashcreate (int size);
 void hashdestroy (hashtable* h);
 
-int hash (hashtable* h, string key);
+int hash (int size, string key);
 
-node* hashtableNewPair (string key, string value);  
+node* hashtableNewPair (string key, string value);
 
 // размещение узла в таблице
 void hashtableSet(hashtable* h, string key , string value);
@@ -35,7 +35,7 @@ hashtable* hashcreate (int size)
     {
         h->table[i] = NULL;
     }
-    h->size = size; 
+    h->size = size;
     return h ;
 }
 
@@ -53,60 +53,71 @@ void hashdestroy (hashtable* h)
     }
 }
 
-int hash(hashtable* h , string key)
+int hash(int size , string key)
 {
-    unsigned long int hashvalue = 0; 
+    unsigned long int hashvalue = 0;
     int i = 0;
     while (hashvalue < ULONG_MAX && i < strlen(key))
     {
-        hashvalue = hashvalue << 8 ; // *256 
+        hashvalue = hashvalue << 8 ; // *256
         hashvalue = hashvalue + key[i];
         i++;
     }
-    return hashvalue % h->size ;
+    return hashvalue % size ;
 }
+
 
 void hashtableSet(hashtable* h, string key , string value)
 {
-    int index = hash(h, key);
+    int index = hash(h->size, key);
     node *new_next;
-    node* last;
+    node* last; // Last valid box(node) in the linked list for given slot
     new_next = h->table[index];
+    last = new_next;
+
+    // Searching the box to modify or add after
     while(new_next != NULL && new_next->key != NULL && new_next->key != key)
     {
         last = new_next;
         new_next = new_next->next;
     }
+
+    // The box to modify
     if(new_next !=NULL && new_next->key != NULL && new_next->key == key)
     {
         new_next->value = value;
     }
+    // The box to add after(append) in the linked list
     else
     {
         node* new_pair = hashtableNewPair(key, value);
-        h->table[index] = new_pair;
+        if (last)
+            last->next = new_pair;
+        else
+            h->table[index] = new_pair;
+        //h->table[index] = new_pair;
     }
 }
+
 
 string hashtableGet (hashtable *h , string key)
 {
     //int tmp = hash(h, key);
     //printf("%d\n", tmp);
-    node *pair = h->table[hash(h, key)];
+    node *pair = h->table[hash(h->size, key)]; //
+
     while(pair != NULL && pair->key != NULL && pair->key != key)
     {
         pair = pair->next;
     }
     if(pair == NULL)
     {
-        return "nothing";
+        return "UNDEFINED";
     }
     else
     {
         return pair->value;
     }
-    
-    
 }
 
 node* hashtableNewPair (string key, string value)
@@ -120,14 +131,19 @@ node* hashtableNewPair (string key, string value)
 
 int main ()
 {
-   hashtable* mytable = hashcreate(100);
-    //node* catnode = hashtableNewPair("cat", "kit");
-    //node* dognode = hashtableNewPair("dog", "pes");
-    hashtableSet(mytable, "cat", "kit");
-    hashtableSet(mytable, "dog", "pes");
-    string catvalue = hashtableGet(mytable, "cat");
-    string dogvalue = hashtableGet(mytable, "elephant");
-    printf("%s\n", catvalue);
-    printf("%s\n", dogvalue);
+    const int hsize = 3;
+    hashtable* mytable = hashcreate(hsize);
+
+    hashtableSet(mytable, "cat", "kit"); // 0
+    hashtableSet(mytable, "dog", "pes"); // 2
+    hashtableSet(mytable, "elephant", "slon"); // 1
+    hashtableSet(mytable, "penguin", "pingvin"); // 0
+    hashtableSet(mytable, "lion", "lev");  // 2
+    hashtableSet(mytable, "tiger", "tigr"); // 0
+
+    printf("Penguin is '%s'\n", hashtableGet(mytable, "penguin"));
+    printf("Cat is '%s'\n", hashtableGet(mytable, "cat"));
+    printf("Elephant is '%s'\n", hashtableGet(mytable, "elephant"));
+    printf("Mouse is '%s'\n", hashtableGet(mytable, "mouse"));
     hashdestroy(mytable);
 }
